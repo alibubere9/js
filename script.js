9a17dbaffee304136431d09b4f2a6dc2c9f35fd6
alibubere9 <alibubere9@gmail.com>
2019-08-19 00:15:43 +0530

    ef,

---
+++ script.js
@@ -0,0 +1,5 @@
+function reg() {
+    var namea = document.getElementsById("ename").value;
+    var emaila = document.getElementsById("eemail").value;
+    alert(" Your name is " + namea + " and your email is " + emaila + ". <br> Are your sure you want to signup");
+}
